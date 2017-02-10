9b8e0029bb55dd1767e4825cfca8f0122d1fff33
Ke.Steve <huangke7296@foxmail.com>
2017-02-10 10:56:33 +0800

    [branch_master] 提交secret文件夹

---
+++ Secreat/Secreat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Secreat
+//
+//  Created by mac on 16/5/28.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#include <iostream>
+#include<stdio.h>
+int main(void){
+    int c=30063,b=2032166262,a=1869357129;
+    printf("%s\n",(char*)&a);
+    return 0;
+}
